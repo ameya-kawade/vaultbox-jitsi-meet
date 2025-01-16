9b10afbe4fdbdcdbce780eb0eb223b116d2425e7
root <root@kali-dell>
2025-01-16 17:46:30 +0530

    stable version

---
+++ token_gen.py
@@ -0,0 +1,38 @@
+import jwt
+import time
+
+def generate_awt_token():
+    # Get current Unix timestamp
+    current_time = int(time.time())
+    
+    # Set token payload
+    payload = {
+        "context": {
+            "user": {
+                "avatar": "your_avatar_url",
+                "name": "your__name",
+                "email": "your__email",
+                "lobby_bypass": False
+            }
+        },
+        "moderator": True,
+        "aud": "jitsi",
+        "iss": "jitsi",
+        "sub": "jitsi-meet.example.com",
+        "room": "*",
+        "nbf": current_time,
+        "exp": current_time + 86400  # 24 hours later
+    }
+
+    # Secret key used for signing the JWT (change to your secret key)
+    secret_key = "secret_token"
+
+    # Generate JWT token using HS256 algorithm
+    token = jwt.encode(payload, secret_key, algorithm="HS256")
+
+    return token
+
+# Generate and print the token
+token = generate_awt_token()
+print(f"Generated JWT Token: {token}")
+
